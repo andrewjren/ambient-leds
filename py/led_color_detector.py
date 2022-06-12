27091cc23cdaa3ef6570ae545923e1766b66bfeb
Andrew Ren <andrewjren@gmail.com>
2022-06-12 16:25:00 -0400

    add camera reference

---
+++ py/led_color_detector.py
@@ -0,0 +1,59 @@
+import cv2
+import numpy as np
+import base64
+import time
+import board
+import neopixel
+
+# gamma 
+gamma_shift = [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+    10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+    17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+    25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+    37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+    51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+    69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+    90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+    115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+    144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+    177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+    215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255]
+
+# led strip
+pixels = neopixel.NeoPixel(board.D18, 30)
+
+# open webcam
+frame_width = 640
+frame_height = 480
+cap = cv2.VideoCapture(0) # open Mac webcam
+cap.set(3, frame_width)
+cap.set(4, frame_height)
+
+first = True
+
+while cap.isOpened():
+    
+    if first:
+        time.sleep(2)
+        first = False
+        print('Finished Sleeping')
+
+    success, img = cap.read()
+    if success:
+        blue = img[240,320,0]
+        green = img[240,320,1]
+        red = img[240,320,2]
+
+        blue_shift = gamma_shift[blue]
+        green_shift = gamma_shift[green]
+        red_shift = gamma_shift[red]
+
+        print('{0},{1},{2}'.format(red_shift,green_shift,blue_shift))
+        pixels.fill((red_shift,green_shift,blue_shift))
+
+cap.release()
+pixels.fill((0,0,0))
