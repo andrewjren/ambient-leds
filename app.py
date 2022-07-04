5143217abb7e4916d36384137adc16d8020755ef
Andrew Ren <andrew@Andrews-MacBook-Pro.local>
2022-07-04 14:22:19 -0400

    updates

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 import os
 
 from flask import Flask, render_template, request, redirect
+from tvleds import tvleds, utilities
 
-# globals
-color = '#0000ff'
+# init AmbientLeds object
+ambient_leds = AmbientLEDs()
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    global color 
     return render_template('base.html', color=color)
 
 @app.route("/fill", methods=['POST'])
 def fill_color():
-    global color
     color = request.form.get('fill_rgb')
-    print(color)
+    #print(color)
+    r,g,b = hex2rgb(color)
+    print('RGB: {0},{1},{2}'.format(r,g,b))
+    ambient_leds.fill(i_red,i_green,i_blue)
     return redirect('/')
